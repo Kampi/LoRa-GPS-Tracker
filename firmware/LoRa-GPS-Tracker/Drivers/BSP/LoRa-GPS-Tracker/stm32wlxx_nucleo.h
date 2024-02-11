d71bd0cc34120ce2896322d1f81b56baa940374e
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2024-02-11 20:06:17 +0100

    Hardware: - Add RESET line for SWD interface - Replace micro USB with USB C
    Software:
    - Add firmware for LoRa-GPS-Tracker PCB
    
    Signed-off-by: Daniel Kampert <DanielKampert@kampis-elektroecke.de>

---
+++ firmware/LoRa-GPS-Tracker/Drivers/BSP/LoRa-GPS-Tracker/stm32wlxx_nucleo.h
@@ -0,0 +1,27 @@
+/**
+  ******************************************************************************
+  * @file    stm32wlxx_nucleo.h
+  * @author  MCD Application Team
+  * @brief   Header for stm32wlxx_nucleo.c
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2020-2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef STM32WLXX_NUCLEO_H
+#define STM32WLXX_NUCLEO_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wlxx_nucleo_errno.h"
+#include "stm32wlxx_nucleo_conf.h"
+
+#endif /* STM32WLXX_NUCLEO_H */
